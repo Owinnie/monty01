d70262b191d4f825806dc2912f054e38162c9c2e
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-06-16 22:16:34 +0300

    3. Implement the swap opcode

---
+++ swap.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+/**
+ * s_wap - swaps first 2 elements of the stack
+ * @stack: double pointer to the first node
+ * @line_number: value of new node
+ */
+void s_wap(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp;
+
+	UNUSED(line_number);
+	if (!(*stack) || !((*stack)->next))
+	{
+		printf("L%u: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	tmp = (*stack)->next;
+	(*stack)->prev = (*stack)->next;
+	(*stack)->next = tmp->next;
+	tmp->prev = NULL;
+	(*stack)->prev = tmp;
+	if (tmp->next)
+		tmp->next->prev = *stack;
+	tmp->next = *stack;
+	(*stack) = (*stack)->prev;
+}
